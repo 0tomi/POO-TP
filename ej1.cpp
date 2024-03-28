549a22185798c2117fe23d2a63be073204d1d47d
0tomi <tomsschlotahuer@hotmail.com>
2024-03-28 05:47:33 -0300

    ds

--- ej1.cpp
+++ ej1.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
+/*
+	Ej1: Usando recursividad para indicar lineas de un archivo 
+*/
 
-int main(int argc, char *argv[]) {
-	
-	cout << "Hello world";
-
+int contarlineas(ifstream &f){
+	string text;
+	if (getline(f,text))
+		return 1 + contarlineas(f);
 	return 0;
 }
 
+int main(int argc, const char** argv) {
+	string direccion;
+	cout << "Indique el nombre del archivo: ";
+	getline(cin,direccion);
+	direccion = "./" + direccion + ".txt";
+
+	ifstream file; file.open(direccion);
+	if (file.fail())
+		return 404;
+	int contador = contarlineas(file);
+	cout << "\nCantidad de lineas: " << contador;
+	return 0;
+}
--- ej5.cpp
+++ ej5.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <fstream>
 
+/*
+    EJ5, sin usar arrays, porque no se ve necesario, y de forma recursiva
+*/
+
 using namespace std;
 
-int main(int argc, char *argv[]){
+int contador(ifstream &f){
+    int nro;
+    string text;
+    if (f >> nro >> nro){
+        getline(f,text);
+        if (nro < 1995)
+            return 1 + contador(f);
+        return contador(f);
+    }
+    return 0;
+}
 
-
+int main(int argc, const char** argv) {
+    int cantidad = 0;
+    ifstream file; file.open("./datos.txt");
+    if (file.fail()){
+        cout << "ERROR: Al abrir el archivo datos.txt";
+        return 404;
+    }
+    cantidad = contador(file);
+    cout << cantidad << " alumnos nacieron antes del anio 1995";
     return 0;
 }
