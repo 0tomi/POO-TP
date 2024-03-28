2c752311a1ba8e9bf0cad230c5e36b5d7b385fdb
0tomi <tomsschlotahuer@hotmail.com>
2024-03-28 05:52:23 -0300

    Update ej1.cpp

--- ej1.cpp
+++ ej1.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 /*
 	Ej1: Usando recursividad para indicar lineas de un archivo 
+	CA01: La consigna no indica el nombre que debe tener el archivo, ni donde debe estar, asi que 
+	le pediremos el nombre del archivo de texto al usuario.
 */
 
 int contarlineas(ifstream &f){
@@ -20,8 +22,10 @@ int main(int argc, const char** argv) {
 	direccion = "./" + direccion + ".txt";
 
 	ifstream file; file.open(direccion);
-	if (file.fail())
+	if (file.fail()){
+		cout << "\nNo se ha podido encontrar el archivo. \nIntente poner el mismo en la misma carpeta donde esta el programa.";
 		return 404;
+	}
 	int contador = contarlineas(file);
 	cout << "\nCantidad de lineas: " << contador;
 	return 0;
