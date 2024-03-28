e48cf2fcd0f66ed566c3bbf057a1495b074f652e
0tomi <tomsschlotahuer@hotmail.com>
2024-03-28 14:51:50 -0300

    ej3

--- ej3.cpp
+++ ej3.cpp
@@ -3,27 +3,39 @@
 
 using namespace std;
 /*
-	3) Escriba un programa que modifique un archivo de texto de forma que cada oración
-    comience en una nueva línea.
+	3) Escriba un programa que modifique un archivo de texto de forma que cada oracion
+    comience en una nueva linea.
 
     CC01: La consigna no aclara el nombre del archivo ni su directorio, 
           asi que se lo pedimos al usuario.
-    CC02: Asumo que vendrá un archivo con puntos que indiquen el fin de una oración, y el objetivo
-          del programa será hacer que comiencen en una nueva línea.
+    CC02: Asumo que vendra un archivo con puntos que indiquen el fin de una oracion, y el objetivo
+          del programa sera hacer que comiencen en una nueva linea.
 */
 
 int main(int argc, char const *argv[])
 {
-    string text;
+    string text = "", aux;
     string direccion;
+	
 	cout << "Indique el nombre del archivo: ";
 	getline(cin,direccion);
 	direccion = "./" + direccion + ".txt";
+	
     ifstream filei; filei.open(direccion);
-    
-    filei.close();
-    ofstream file; file.open(direccion,ios::app);   
-   
-    file.close();
+    if (filei.fail())
+		return 404;
+	else {			// esto va en el else as� en caso de no estar, no se crea un archivo.
+		getline(filei,aux,'.');
+		while (!filei.eof()){
+			text += aux + ".\n";
+			getline(filei,aux,'.');
+		}
+		
+		ofstream file; file.open(direccion);   
+			file << text;
+		file.close();
+	}
+	
+	filei.close();
     return 0;
 }
