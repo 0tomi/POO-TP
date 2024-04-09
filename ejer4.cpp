194b152b3f90741a6cc62c9d88e89f6861178312
aguspee <165193270+aguspee@users.noreply.github.com>
2024-04-09 17:01:06 -0300

    Update ejer4.cpp

--- ejer4.cpp
+++ ejer4.cpp
@@ -7,9 +7,51 @@ reemplazó.*/
 #include <fstream>
 
 using namespace std;
-
+string reemplaza(char c1, char c2, string renglon, int &l){
+	
+	for (char& c : renglon) {
+		if (c == c1) {
+			c=c2;
+			l++;
+			
+		}
+	}
+	return  renglon;
+}
 int main(int argc, char const *argv[])
 {
-    
+    string direccion, renglon, texto, linea;
+		string  texo ="";
+		char car, car2;
+		int a=0;
+		cout << "Indique el nombre del archivo: ";
+		getline(cin,direccion);
+		direccion = "./" + direccion + ".txt";
+		ifstream arc;
+		arc.open(direccion);
+		
+		if (arc.fail()){
+			cout<<"Error en la apertura del archivo. ";
+		}
+		else {
+			cout<<"Ingrese caracter a buscar: "<<endl;
+			cin>>car;
+			cout<<"Ingrese caracter oara reemplazar: "<<endl;
+			cin>>car2;
+			
+			while(getline(arc, renglon)){
+				int l=0;
+				linea=reemplaza(car, car2, renglon, l);
+				texto +=linea+"\n";
+				cout<<"Linea "<<a<<" "<<l<<" veces reemplazada"<<endl;
+				a++;
+			}
+			ofstream file;
+			file.open(direccion);
+			file<<texto<<endl;
+			file.close();
+		}
+		arc.close();
+		
     return 0;
 }
