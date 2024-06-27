09a323c39621ef8f1d3d4ab7465a2ebe5e280923
0tomi <tomsschlotahuer@hotmail.com>
2024-06-27 17:52:03 -0300

    Create reglas.h

---
+++ TP2/reglas.h
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+class reglasNivel1{
+    public:
+    // constructores:
+        reglasNivel1();
+    // metodos:
+        void resetReglas();
+
+    private:
+        int fecha; // va a ser solo el anio
+        int duracionDeEstancia;
+        string* tipoDeVisita;
+        string* nacionalidad;
+        string* EstadoCivil;
+};
