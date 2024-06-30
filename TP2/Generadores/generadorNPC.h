1cb092ba13f712039466f733d4baa9adaaaf36a0
0tomi <tomsschlotahuer@hotmail.com>
2024-06-30 18:27:13 -0300

    test

--- TP2/Generadores/generadorNPC.h
+++ TP2/Generadores/generadorNPC.h
@@ -2,8 +2,21 @@
 
 #ifndef GENNPC_H
 #define GENNPC_H
+
+#include <string>
+using namespace std;
+
 class GeneradorNPC{
-    
+public:
+    GeneradorNPC();
+    NPC* getNPC();
+private:
+    // NPC que se va a generar
+    NPC* NPC2create;
+
+    // Array con direcciones donde se encuentran las caras de los npcs
+    string* ImagenesNPCS;
+    char generos[3] = {'M', 'H', 'X'};
 };
 
 #endif // !GENNPC_H
--- TP2/Juego.cpp
+++ TP2/Juego.cpp
@@ -1,11 +1,13 @@
 #include <fstream>
+#include "LectorArchivos.cpp"
 #include "Juego.h"
 
 Juego::Juego(){
     this->atributos = new AtributosComunes();
 
     // Leemos los paises y los guardamos en el array de paises
-    this->LeerPaises();
+    LectorArchivos LA("ArchivosTexto/paises.txt");
+    this->atributos->setAtributos(LA.getArray(), LA.getTopeArray());
 
     // Seteamos las reglas del juego, pasando el array de paises.
     this->rules = new reglas(atributos);
