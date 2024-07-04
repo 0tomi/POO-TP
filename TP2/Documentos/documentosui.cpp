5f8bd72cfad18395d97055797c413047138cc0e4
0tomi <tomsschlotahuer@hotmail.com>
2024-07-04 01:53:26 -0300

    dioss

--- TP2/Documentos/documentosui.cpp
+++ TP2/Documentos/documentosui.cpp
@@ -16,22 +16,17 @@ DocumentosUI::~DocumentosUI()
     delete ui;
 }
 
-void DocumentosUI::clickIzqEvent(QMouseEvent *event) {
-    // Si se preciona el clic izquierdo
+void DocumentosUI::mousePressEvent(QMouseEvent *event) {
     if (event->button() == Qt::LeftButton) {
-        // Captamos la posicion del mouse
         m_dragStartPosition = event->pos();
-        // Mostramos el widget por encima de los demas
         raise();
     }
 }
 
 void DocumentosUI::mouseMoveEvent(QMouseEvent *event) {
     if (event->buttons() & Qt::LeftButton) {
-        // Obtenemos las cordenadas desde donde se esta moviendo el mouse
         int dx = event->pos().x() - m_dragStartPosition.x();
         int dy = event->pos().y() - m_dragStartPosition.y();
-        // Movemos el widget a esas posiciones
         move(pos() + QPoint(dx, dy));
     }
 }
