723055173efc33fa4bf98813d2847a7e4743cd12
0tomi <tomsschlotahuer@hotmail.com>
2024-07-08 19:56:28 -0300

    oka

--- TP2UI/build-TP2-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/qtcsettings.cmake
+++ TP2UI/build-TP2-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -1,2 +1,10 @@
 # This file is managed by Qt Creator, do not edit!
 
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/TomsS/Documents/C++/POO/POO-TP/TP2UI/build-TP2-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "B:/QT/6.6.2/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "B:/QT/6.6.2/mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "B:/QT/Tools/mingw1120_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "B:/QT/Tools/mingw1120_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
