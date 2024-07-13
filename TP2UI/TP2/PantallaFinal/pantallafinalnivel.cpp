19fa11eb65ee9072ff9fd8e6d5b4c80046ca31e4
Joaquín Nicolás Carena <89748758+joacocarena@users.noreply.github.com>
2024-07-13 15:12:42 -0300

    emit signals hecho

---
+++ TP2UI/TP2/PantallaFinal/pantallafinalnivel.cpp
@@ -0,0 +1,36 @@
+#include "pantallafinalnivel.h"
+#include "ui_pantallafinalnivel.h"
+
+#include <QDebug>
+
+PantallaFinalNivel::PantallaFinalNivel(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::PantallaFinalNivel)
+{
+    ui->setupUi(this);
+}
+
+PantallaFinalNivel::~PantallaFinalNivel()
+{
+    delete ui;
+}
+
+void PantallaFinalNivel::setPantallaFinalUI(Juego* juegoInfo) {
+    ui->credsSocObts->setText(QString("%1").arg(juegoInfo->getSocialCreditsEarnedInLevel()));
+    ui->multasObt->setText(QString("%1").arg(juegoInfo->getMultas()));
+
+    // conecc.signal de salirBtn:
+    connect(ui->salirBtn, &QPushButton::clicked, this, &PantallaFinalNivel::on_salir_clicked);
+    // connec. signal de sigNivelBtn:
+    connect(ui->sigNivelBtn, &QPushButton::clicked, this, &PantallaFinalNivel::on_sigNivel_clicked);
+}
+
+void PantallaFinalNivel::on_salir_clicked() {
+    emit salirClicked();
+    qDebug() << "SALIR btn presionado y señal emitida";
+}
+
+void PantallaFinalNivel::on_sigNivel_clicked() {
+    emit sigNivelClicked();
+    qDebug() << "SIG NIVEL btn presionado y señal emitida";
+}
---
+++ TP2UI/TP2/PantallaFinal/pantallafinalnivel.h
@@ -0,0 +1,31 @@
+#ifndef PANTALLAFINALNIVEL_H
+#define PANTALLAFINALNIVEL_H
+
+#include "../juego.h"
+
+#include <QWidget>
+#include <QString>
+
+namespace Ui {
+class PantallaFinalNivel;
+}
+
+class PantallaFinalNivel : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit PantallaFinalNivel(QWidget *parent = nullptr);
+    ~PantallaFinalNivel();
+    void setPantallaFinalUI(Juego* juegoInfo);
+signals:
+    void salirClicked();
+    void sigNivelClicked();
+private slots:
+    void on_salir_clicked();
+    void on_sigNivel_clicked();
+private:
+    Ui::PantallaFinalNivel *ui;
+};
+
+#endif // PANTALLAFINALNIVEL_H
