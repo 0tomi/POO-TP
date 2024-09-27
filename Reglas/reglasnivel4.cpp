6b02aa8cba4ae00937acec94115a4bb5fb5830eb
Joaquín Nicolás Carena <89748758+joacocarena@users.noreply.github.com>
2024-09-26 21:39:06 -0300

    primera implementacion reglas lvl4

--- Reglas/reglasnivel4.cpp
+++ Reglas/reglasnivel4.cpp
@@ -1,6 +1,51 @@
 #include "reglasnivel4.h"
+#include <QTime>
 
-ReglasNivel4::ReglasNivel4()
-{
+ReglasNivel4::ReglasNivel4(): Reglas(), Random(QTime::currentTime().msec()) {}
 
+void ReglasNivel4::generarPaisesPasoValidos(int cant, vector<QString>& listaPaisesPasoValidos) {
+    //if (!ValidarDatos())
+    paisesPasoValidos.clear();
+    
+    if (cant < 1)
+        cant = Random.bounded(listaPaisesPasoValidos.size()) + 1;
+    
+    paisesPasoValidos.resize(cant);
+    
+    for (int i = 0; i < cant; i++) {
+        int listLeng = listaPaisesPasoValidos.size();
+        paisesPasoValidos[i] = listaPaisesPasoValidos[Random.bounded(listLeng)];
+    }
+}
+
+void ReglasNivel4::generarBienesTranspValidos(int cant, vector<QString> &listaBienesTranspValidos) {
+    bienesTranspValidos.clear();
+    
+    if (cant < 1)
+        cant = Random.bounded(listaBienesTranspValidos.size()) + 1;
+    
+    bienesTranspValidos.resize(cant);
+    
+    for (int i = 0; i < cant; i++) {
+        int listLeng = listaBienesTranspValidos.size();
+        bienesTranspValidos[i] = listaBienesTranspValidos[Random.bounded(listLeng)];
+    }
+}
+
+void ReglasNivel4::generarOcupacionValida(vector<QString> ocupacionesValidas) {
+    ocupacion = ocupacionesValidas[Random.bounded(ocupacionesValidas.size())];
+}
+
+
+// getters
+vector<QString> ReglasNivel4::getPaisesPasoValidos() const {
+    return paisesPasoValidos;
+}
+
+vector<QString> ReglasNivel4::getBienesTranspValidos() const {
+    return bienesTranspValidos;
+}
+
+QString ReglasNivel4::getOcup() {
+    return ocupacion;
 }
--- Reglas/reglasnivel4.h
+++ Reglas/reglasnivel4.h
@@ -2,10 +2,30 @@
 #define REGLASNIVEL4_H
 #include "reglas.h"
 
+#include <vector>
+#include <QString>
+#include <QRandomGenerator>
+
+using namespace std;
+
 class ReglasNivel4: public Reglas
 {
+private:
+    vector<QString> paisesPasoValidos;
+    vector<QString> bienesTranspValidos;
+    QString ocupacion;
+
+    QRandomGenerator Random;
 public:
     ReglasNivel4();
+    void generarPaisesPasoValidos(int cant, vector<QString>& listaPaisesPasoValidos);
+    void generarBienesTranspValidos(int cant, vector<QString>& listaBienesTranspValidos);
+    void generarOcupacionValida(vector<QString> ocupacionesValidas);
+
+    // getters
+    vector<QString> getPaisesPasoValidos() const;
+    vector<QString> getBienesTranspValidos() const;
+    QString getOcup();
 };
 
 #endif // REGLASNIVEL4_H
