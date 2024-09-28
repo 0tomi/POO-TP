87c1b50d5bbd7b4c5f0c5210a34a4897e0ebb6c8
0tomi <tomsschlotahuer@hotmail.com>
2024-09-28 01:05:40 -0300

    .

--- Reglas/reglasnivel4.cpp
+++ Reglas/reglasnivel4.cpp
@@ -1,4 +1,37 @@
 #include "reglasnivel4.h"
 #include <QTime>
 
-ReglasNivel4::ReglasNivel4(){}
+ReglasNivel4::ReglasNivel4(): Reglas(), Random(QTime::currentTime().msec())
+{
+
+}
+
+std::vector<QString> ReglasNivel4::getPaisesPermitidos() const
+{
+    return PaisesPermitidos;
+}
+
+std::vector<QString> ReglasNivel4::getPaisesNoPermitidos() const
+{
+    return PaisesNoPermitidos;
+}
+
+std::vector<QString> ReglasNivel4::getOcupacionPermitidos() const
+{
+    return OcupacionPermitidos;
+}
+
+std::vector<QString> ReglasNivel4::getOcupacionNoPermitidos() const
+{
+    return OcupacionNoPermitidos;
+}
+
+std::vector<QString> ReglasNivel4::getBienesTransportadosPermitidos() const
+{
+    return BienesTransportadosPermitidos;
+}
+
+std::vector<QString> ReglasNivel4::getBienesTransportadosNoPermitidos() const
+{
+    return BienesTransportadosNoPermitidos;
+}
--- Reglas/reglasnivel4.h
+++ Reglas/reglasnivel4.h
@@ -1,11 +1,36 @@
 #ifndef REGLASNIVEL4_H
 #define REGLASNIVEL4_H
 #include "reglas.h"
+#include <QRandomGenerator>
 
+// Las reglas de la estancia se sacaran de las reglas de nivel 1
 class ReglasNivel4: public Reglas
 {
 public:
     ReglasNivel4();
+    void setSeed (int Seed);
+    void generar_PaisesPaso(int CantPermitida);
+    void generar_PaisesPaso(std::vector<QString> ListaPaisesPermitidos);
+    void generar_Ocupacion(int CantPermitida);
+    void generar_Ocupacion(std::vector<QString> ListaOcupacionPermitidos);
+    void generar_BienesTransportados(int CantPermitida);
+    void generar_BienesTransportados(std::vector<QString> ListaBienesTransportadosPermitidos);
+
+    std::vector<QString> getPaisesPermitidos() const;
+    std::vector<QString> getPaisesNoPermitidos() const;
+    std::vector<QString> getOcupacionPermitidos() const;
+    std::vector<QString> getOcupacionNoPermitidos() const;
+    std::vector<QString> getBienesTransportadosPermitidos() const;
+    std::vector<QString> getBienesTransportadosNoPermitidos() const;
+
+private:
+    std::vector<QString> PaisesPermitidos;
+    std::vector<QString> PaisesNoPermitidos;
+    std::vector<QString> OcupacionPermitidos;
+    std::vector<QString> OcupacionNoPermitidos;
+    std::vector<QString> BienesTransportadosPermitidos;
+    std::vector<QString> BienesTransportadosNoPermitidos;
+    QRandomGenerator Random;
 };
 
 #endif // REGLASNIVEL4_H
