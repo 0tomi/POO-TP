73c2a17563ed5ca683f70fe06b46f8977f3a83b3
0tomi <tomsschlotahuer@hotmail.com>
2024-09-28 23:50:18 -0300

    .

--- Reglas/reglas.cpp
+++ Reglas/reglas.cpp
@@ -107,3 +107,108 @@ bool Reglas::ValidarNombres(vector<QString>& Lista, QString * Lista2, int max)
     return true;
 }
 
+
+void Reglas::crearParDatos(std::vector<QString> &vectorOriginal, std::vector<parDatos> &vectorPar)
+{
+    for (const auto& elemento: vectorOriginal)
+        vectorPar.push_back({elemento, false});
+}
+
+void Reglas::resetearParDatos(std::vector<parDatos> &vectorPar)
+{
+    for (auto& elemento: vectorPar)
+        elemento.Insertado = false;
+}
+
+int Reglas::checkCondiciones(int cantidad, int min, std::vector<parDatos>& vec)
+{
+    if (cantidad < 0 || cantidad > vec.size())
+        cantidad = rand.bounded(vec.size());
+
+    return cantidad;
+}
+
+bool Reglas::checkCondiciones(int min, std::vector<parDatos> &vec, std::vector<QString> &lista)
+{
+    auto tamanioLista = lista.size();
+    if (tamanioLista < min || tamanioLista > vec.size())
+        return false;
+    if (!checkTipado(vec, lista))
+        return false;
+
+    return true;
+}
+
+bool Reglas::checkTipado(std::vector<parDatos> &vec, std::vector<QString> &lista)
+{
+    bool ListaCorrecta = true;
+    for (auto& elemento: lista)
+        if (!checkTipado(elemento, vec))
+            ListaCorrecta = false;
+
+    return ListaCorrecta;
+}
+
+bool Reglas::checkTipado(QString &elemento, std::vector<parDatos> &vec)
+{
+    QString palabra1 = elemento.toLower(); QString palabra2;
+    for (auto& element: vec){
+        palabra2 = element.dato.toLower();
+        if (palabra1 == palabra2){
+            element.Insertado = true;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+std::list<QString> Reglas::generarPermitido(int cantidad, std::vector<parDatos> &vec)
+{
+    std::vector<QString> VectorPermitido;
+    VectorPermitido.resize(cantidad);
+
+    int CantidadInsertada = 0; int tamanio = vec.size(); int indice_random;
+    while (cantidad){
+        do{
+            indice_random = rand.bounded(tamanio);
+            VectorPermitido[CantidadInsertada] = vec[indice_random].dato;
+        }while(vec[indice_random].Insertado);
+
+        vec[indice_random].Insertado = true;
+        cantidad--; CantidadInsertada++;
+    }
+
+    std::list<QString> Lista2Return;
+    for (const auto& elemento: VectorPermitido)
+        Lista2Return.push_back(elemento);
+
+    return Lista2Return;
+}
+
+std::list<QString> Reglas::generarPermitido(std::vector<QString> &Permitidos)
+{
+    std::list<QString> Lista2Return;
+    for (const auto& elemento: Permitidos)
+        Lista2Return.push_back(elemento);
+
+    return Lista2Return;
+}
+
+std::list<QString> Reglas::generarNoPermitido(std::vector<parDatos> &total)
+{
+    std::list<QString> listaNoPermitidos;
+    for (auto& elemento: total)
+        if (!elemento.Insertado)
+            listaNoPermitidos.push_back(elemento.dato);
+
+    return listaNoPermitidos;
+}
+
+std::vector<QString> Reglas::generarVector(const std::list<QString> &lista) const
+{
+    std::vector<QString> newVector;
+    for (const QString& element: lista)
+        newVector.push_back(element);
+    return newVector;
+}
--- Reglas/reglasnivel5.cpp
+++ Reglas/reglasnivel5.cpp
@@ -4,3 +4,8 @@ ReglasNivel5::ReglasNivel5()
 {
     rand.seed(QTime::currentTime().msec());
 }
+
+std::vector<QString> ReglasNivel5::getObjetos() const
+{
+    return Objetos;
+}
